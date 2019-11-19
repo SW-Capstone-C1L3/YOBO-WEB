f5436f4fc3d0497fd9094b4a2929d0faea95c424
dddff22 <dddfff22@gmail.com>
2019-11-19 17:34:54 +0900

    add main dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter,Switch, Route } from "react-router-dom";
-import { Main, Auth, NotFound } from "../pages";
+import { Main, Auth, NotFound,DashBoard } from "../pages";
 
 class App extends Component {
   render() {
@@ -10,6 +10,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact={true} component={Main} />
           <Route path="/auth/:kind" exact={true} component={Auth} />
+          <Route path="/main/dashboard" exact={true} component={DashBoard} />
           <Route component={NotFound} />
         </Switch>
         </BrowserRouter>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as Auth } from './Auth';
 export { default as Main } from "./Main";
 export { default as NotFound } from "./NotFound";
+export { default as DashBoard } from "./DashBoard";
